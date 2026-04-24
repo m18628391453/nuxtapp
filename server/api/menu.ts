ccf598cd5cd1fc2156e70cb8bd7feb2b7cfc6074
zhaozy <Aria_Miazzy@noreply.gitcode.com>
2026-04-24 11:43:49 +0800

    fix: 代码调整；

--- server/api/menu.ts
+++ server/api/menu.ts
@@ -52,14 +52,21 @@ export default defineEventHandler(async (event) => {
         subMenu: [
           { name: '负荷监测', route: '/powerload/monitor', icon: 'GalleryThumbnails', index: 0 },
           { name: '负荷建模', route: '/powerload/model', icon: 'Boxes', index: 1 },
-          { name: '负荷分析', route: '/powerload/analyse', icon: 'ChartNoAxesCombined', index: 2 },
+          { name: '负荷配置', route: '/powerload/device', icon: 'Boxes', index: 2 },
+          { name: '负荷分析', route: '/powerload/analyse', icon: 'ChartNoAxesCombined', index: 3 },
         ] 
       },
       { name: '预测管理', route: '/forecast', icon: 'TrendingUp', index: 4, subMenu: [] },
       { name: '策略管理', route: '/strategy', icon: 'Sliders', index: 5, subMenu: [] },
       { name: '能碳管理', route: '/carbon', icon: 'Cloud', index: 6, subMenu: [] },
       { name: '电力交易', route: '/trade', icon: 'BarChart3', index: 7, subMenu: [] },
-      { name: '基础设置', route: '/base', icon: 'Settings', index: 8, subMenu: [] },
+      { name: '基础设置', route: '/base', icon: 'Settings', index: 8, 
+        subMenu: [
+          { name: '园区管理', route: '/base/park', icon: 'User', index: 0 },
+          { name: '分区管理', route: '/base/area', icon: 'User', index: 1 },
+          { name: '设备类型管理', route: '/base/devicetype', icon: 'User', index: 2 },
+        ]
+       },
       { name: '系统设置', route: '/system', icon: 'Shield', index: 9, subMenu: [] },
     ]
   
