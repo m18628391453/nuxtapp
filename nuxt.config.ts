759f36fa8d9f2379f949c9263e0b206874f8eaa6
zhaozy <Aria_Miazzy@noreply.gitcode.com>
2026-05-12 14:59:38 +0800

    fix: 删除依赖；

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,6 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@ant-design-vue/nuxt',
-    '@vercel/analytics',
-    '@vercel/speed-insights',
   ],
   css: [
     '~/assets/css/global.css',
